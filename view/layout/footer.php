ee87bcf84d73e5967b166e13d83f51f8c1519211
Alexander Ivanov <vsehsvt@bk.ru>
2021-03-28 19:57:11 +0300

    Added copyright in footer

--- view/layout/footer.php
+++ view/layout/footer.php
@@ -1,9 +1,12 @@
 <div id="footer">
     <div class="copy">
+
     </div>
     <div class="flinks">
+
         <span><a href="/">Главная</a></span><span class="separator"> | </span>
-        <span><a href="/conract">Связь</a></span>
+        <span><a href="/contact">Связь</a></span><span class="separator"> | </span>
+        <span><a href="#">&#169; 2021</a></span>
     </div>
     <br/><br/>
 
